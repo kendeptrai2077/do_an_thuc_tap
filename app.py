261c1deac51b2d2664538a85391e0be68f4f6982
kendeptrai2077 <120314202+kendeptrai2077@users.noreply.github.com>
2025-04-01 14:19:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,5 +74,4 @@ def upload():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Đổi mặc định thành 10000
-    app.run(host="0.0.0.0", port=port, debug=False)  # Tắt debug trong production
+    app.run()  # Tắt debug trong production
